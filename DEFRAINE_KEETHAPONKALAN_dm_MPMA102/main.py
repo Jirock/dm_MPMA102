1f0b9f89e0b5ad780fb2f2dd7e9e14ab778d0875
Jorick <34106142+Jirock@users.noreply.github.com>
2019-03-11 14:30:45 +0100

    upload all features

---
+++ DEFRAINE_KEETHAPONKALAN_dm_MPMA102/main.py
@@ -0,0 +1,27 @@
+from decimal import Decimal
+
+
+# Q3
+def factorielle(n):
+    """
+    fonction récusirve
+    :param n: entier naturel
+    :return: factorielle de n
+    """
+    if n == 1:
+        return n
+    else:
+        return n * factorielle(n - 1)
+
+
+# Q4
+def question4():
+    N = 0
+    S = 1 / ((2 * N + 3) * factorielle(N + 1))
+    while S > 0.000001:
+        N += 1
+        S = 1 / ((2 * N + 3) * Decimal(factorielle(N + 1)))
+    return N
+
+
+print(question4())
